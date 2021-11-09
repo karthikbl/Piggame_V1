0569d0a1686d05228e46a8864ebc2c2dee5b4d3c
karthik <kblswami@gmail.com>
2021-11-09 21:44:13 +0530

    initial commit

---
+++ header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default header
+
+
+input: [1,2,3,4,5,6,4,3,2] 
+
+
+
+output: [1,2,3,4,5,6];
+Suresh J17:17
+let products=[  
+{id:1,name:'Santoor',price:30},  
+{id:2,name:'Dettol',price:60},  
+{id:3,name:'Savlon',price:90},
+{id:4,name:'Jhonson & Johnson',price:120}
+];
+
+//1. Extract an array with only names Ans: ['Santoor','Dettiol','Savlon','Jhonson & Johnson']
+// 2. Get the name of the product with Price=90 , Ans: Savlon
+//3. Get the sum of all prices Ans: 300
---
+++ index.js
@@ -0,0 +1,10 @@
+import react from 'REACT'
+import header from './header'
+
+
+
+
+reactDom.render
+ (<app.js>, document.getelementbyid(root))
+
+ <header type= {type01} > We will type react</header>
